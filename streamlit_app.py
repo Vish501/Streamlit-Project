6315d90587da8fc004a3b1fafbeae9cbf4656e8b
Vish501 <76504332+Vish501@users.noreply.github.com>
2024-12-04 12:02:12 +0000

    Temp File updated with Project 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,71 +1,53 @@
+from datetime import datetime
 import pandas as pd
 import streamlit as st
 
+@st.cache_data
+def data_loader(year: int):
+    # Loading the data from the website and caching it to prevent multiple queries
+    # Also dropping the Rk column, since Pandas is already maintinaing it
+    url = f'https://www.basketball-reference.com/leagues/NBA_{year}_per_game.html'
+    data = pd.read_html(url)[0]
+    data = data.drop(['Rk'], axis = 1)
+    data = data[data['Team'].notna()]
+    data.index += 1
+    return data
 
-def checker(dna_sequence: str) -> bool:
-    # Checks if the dna_sequence is a valid sequence, i.e. only contains adenine (A), cytosine (C), guanine (G), and thymine (T).
-    dna = ''
-    for i in dna_sequence:
-        if i not in ['A', 'C', 'G', 'T']:
-            dna += i
-    return len(dna) == 0
 
-
-def seq_to_dict(dna_sequence: str) -> dict:
-    # Convert the dna_sequence into a dictionary
-    dna_dic = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
-    for i in dna_sequence:
-        dna_dic[i] += 1
-    return dna_dic
+def csv_download(data):
+    return data.to_csv().encode('utf-8')
 
 
 def main():
     # Renders the page heading and subheading for the web application
     st.write('''
-    # DNA Nucleotide Analyzer
-    Counts the nucleotide compotion of am inputted DNA query
+    # NBA Player Stats Explorer
+    ###### Data Source: https://www.basketball-reference.com/
     ''')
 
 
-    # Input section for the web application
-    temp_text = 'GAACACGTGGAGGCAAACAGGAAGGTGAAGAAGAACTTATCCTATCAGGACGGAAGGTCCTGTGCTCGGG'
-    dna_sequence = st.text_area('Enter DNA Sequence', temp_text, height = 68)
-    dna_sequence = dna_sequence.replace(' ', '').upper()
+    # Input Selection Menu (Year, Team, Position)
+    # Also loading the data using year to help build Team and Position lists
+    st.sidebar.header('Filters')
+    year = st.sidebar.selectbox('Year', list(range(1947, datetime.now().year))[::-1])
 
+    player_stats = data_loader(year)
+    available_teams = player_stats.Team.unique()
+    available_positions = player_stats.Pos.unique()
     
-    # Input for the numbers of nucletoides to be used in the positional analysis
-    n = st.slider('Group size of nucleotides for positional analysis', min_value = 2, max_value = 12)
-
-
-    # Validates if the received sequence is a usuable DNA sequence and triggers the Output based on it
-    flag = True
-
-    if len(dna_sequence) == 0 or checker(dna_sequence) == False:
-        st.markdown('**Error: Invalid Sequence Received (Issue with lenght or content)**')
-        flag = False
-    else:
-        st.markdown('Recevied Sequence: ' + dna_sequence)
-
+    teams = st.sidebar.multiselect('Team', available_teams, available_teams)
+    positions = st.sidebar.multiselect('Position', available_positions, available_positions)
     
-    # Renders the output chart
-    if flag == True:
-        st.markdown('***')
-        st.markdown('### DNA Analysis')
+
+    # Filtering the data and rendering it
+    player_stats_selected = player_stats[player_stats.Team.isin(teams) & player_stats.Pos.isin(positions)]
+    st.header('Stats of the Filtered Team')
+    st.write(f'Size: **{player_stats_selected.shape[0]} players** and their **{player_stats_selected.shape[1]} data points.**')
+    st.dataframe(player_stats_selected)
 
 
-        st.markdown('###### Nucleotide Count')
-        dna_dic = seq_to_dict(dna_sequence)
-        st.bar_chart(dna_dic, x_label = 'Nucleotides', y_label = 'Count')
-
-
-        st.markdown('###### Nucleotide Positional %')
-        st.markdown('_Note: In case the provide sequence cannot be bifurcated into equal parts the last part will be dropped_')
-        positional_seq = [dna_sequence[i:i+n] for i in range(0, len(dna_sequence) - len(dna_sequence) % n, n)]
-        df = pd.DataFrame({'Col': positional_seq})
-        df = df.Col.str.split('', n = n, expand = True).drop(0, axis = 1)
-        df = df.apply(pd.Series.value_counts)
-        df = df.apply(lambda x: (x/x.sum())*100).round(2).fillna(0)
-        st.write(df)
+    # Allowing for the filtered data to be downloaded
+    st.download_button('Press to Download the Filtered Data', csv_download(player_stats_selected), 'basketball_player_data.csv', 'text/csv', key = 'download-csv')
 
 
 if __name__ == '__main__':
