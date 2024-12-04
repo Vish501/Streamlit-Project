4693efc8fd6dd39c5b37776abaed82b76aa2cfe9
Vish501 <76504332+Vish501@users.noreply.github.com>
2024-12-04 14:21:06 +0000

    Update for Project 4 - Stock Packing Analyzer

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,54 +1,38 @@
-from datetime import datetime
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import base64
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import yfinance as yf
+
 
 @st.cache_data
-def data_loader(year: int):
-    # Loading the data from the website and caching it to prevent multiple queries
-    # Also dropping the Rk column, since Pandas is already maintinaing it
-    url = f'https://www.basketball-reference.com/leagues/NBA_{year}_per_game.html'
+def load_data():
+    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
     data = pd.read_html(url)[0]
-    data = data.drop(['Rk'], axis = 1)
-    data = data[data['Team'].notna()]
-    data.index += 1
+    data['Headquarters Location'] = data['Headquarters Location'].str.split(';', expand = True)[0]
+    data[['HQ City', 'HQ State']] = data['Headquarters Location'].str.split(', ', n = 1, expand = True)
     return data
-
-
-def csv_download(data):
-    return data.to_csv().encode('utf-8')
 
 
 def main():
     # Renders the page heading and subheading for the web application
-    st.write('''
-    # NBA Player Stats Explorer
-    ###### Data Source: https://www.basketball-reference.com/
+    st.markdown('''
+    # Stock Bubbles
+    **Data Source: https://en.wikipedia.org/wiki/List_of_S%26P_500_companies**
     ''')
 
-
-    # Input Selection Menu (Year, Team, Position)
-    # Also loading the data using year to help build Team and Position lists
+    # Setting up User filter options
     st.sidebar.header('Filters')
-    year = st.sidebar.selectbox('Year', list(range(1947, datetime.now().year))[::-1])
-
-    player_stats = data_loader(year)
-    available_teams = player_stats.Team.unique()
-    available_positions = player_stats.Pos.unique()
-    
-    teams = st.sidebar.multiselect('Team', available_teams, available_teams)
-    positions = st.sidebar.multiselect('Position', available_positions, available_positions)
-    
-
-    # Filtering the data and rendering it
-    player_stats_selected = player_stats[player_stats.Team.isin(teams) & player_stats.Pos.isin(positions)]
-    st.header('Stats of the Filtered Team')
-    st.write(f'Size: **{player_stats_selected.shape[0]} players** and their **{player_stats_selected.shape[1]} data points.**')
-    st.dataframe(player_stats_selected)
+    stock_data = load_data()
+    sectors = stock_data['GICS Sector'].unique()
+    sub_sectors = stock_data['GICS Sub-Industry'].unique()
+    city = stock_data['HQ City'].unique()
+    state = stock_data['HQ State'].unique()
 
 
-    # Allowing for the filtered data to be downloaded
-    st.download_button('Press to Download the Filtered Data', csv_download(player_stats_selected), 'basketball_player_data.csv', 'text/csv', key = 'download-csv')
-
+    stock_data
 
 if __name__ == '__main__':
     main()
